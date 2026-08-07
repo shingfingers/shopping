c3ec6ffe6220f7c10eb8c8af977c3624dddf4833
tanzixuan <3367815653@qq.com>
2026-08-07 08:43:17 +0800

    message_api 消息接口

---
+++ message_api/src/main/java/com/bk/message_api/MessageApiApplication.java
@@ -0,0 +1,21 @@
+package com.bk.message_api;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableDubbo
+@RefreshScope
+public class MessageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApiApplication.class, args);
+    }
+
+}
