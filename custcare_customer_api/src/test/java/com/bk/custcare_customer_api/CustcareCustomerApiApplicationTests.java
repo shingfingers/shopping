64a5d9f7b6cc600d6a712ab4c04472edc89a4a2a
tanzixuan <3367815653@qq.com>
2026-08-07 08:43:17 +0800

    custcare_customer_api AI 客服接口

---
+++ custcare_customer_api/src/test/java/com/bk/custcare_customer_api/CustcareCustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.custcare_customer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustcareCustomerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
