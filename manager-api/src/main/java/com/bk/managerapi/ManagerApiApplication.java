3961991b4930720721e04e2ebb784b3b5630e4cf
tanzixuan <3367815653@qq.com>
2026-08-07 08:43:17 +0800

    manager-api 管理后台接口

---
+++ manager-api/src/main/java/com/bk/managerapi/ManagerApiApplication.java
@@ -0,0 +1,17 @@
+package com.bk.managerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient //向注册中心注册该服务
+@RefreshScope // 配置动态刷新
+public class ManagerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApiApplication.class, args);
+    }
+}
+
