4226464c60f7345a61f3eb65a4ff80f41f92830f
tanzixuan <3367815653@qq.com>
2026-08-17 15:01:58 +0800

    admin_service 管理员服务

--- admin_service/src/main/java/com/bk/admin_service/AdminServiceApplication.java
+++ admin_service/src/main/java/com/bk/admin_service/AdminServiceApplication.java
@@ -3,11 +3,15 @@ package com.bk.admin_service;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@EnableDubbo // 开启Dubbo
 @SpringBootApplication
+@MapperScan("com.bk.admin_service.mapper") // 扫描 MyBatis Mapper 接口
 public class AdminServiceApplication {
 
     public static void main(String[] args) {
